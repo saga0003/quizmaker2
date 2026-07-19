0ff5c8756070ebd832c42776c62c2d899fbe3aae
saga0003 <sagar@smis.edu.in>
2026-07-19 21:51:54 +0530

    Use cloud-aware school benchmark workspace

--- src/app/school/benchmarks/page.tsx
+++ src/app/school/benchmarks/page.tsx
@@ -1,5 +1,5 @@
 import { DashboardShell } from "@/components/DashboardShell";
 import { ProtectedPage } from "@/components/ProtectedPage";
-import { SchoolBenchmarkWorkspace } from "@/components/benchmarks/SchoolBenchmarkWorkspace";
+import { SchoolBenchmarkRouter } from "@/components/benchmarks/BenchmarkWorkspaceRouter";
 
-export default function Page(){return <ProtectedPage allowed="school"><DashboardShell kind="school"><SchoolBenchmarkWorkspace/></DashboardShell></ProtectedPage>}
+export default function Page(){return <ProtectedPage allowed="school"><DashboardShell kind="school"><SchoolBenchmarkRouter/></DashboardShell></ProtectedPage>}
