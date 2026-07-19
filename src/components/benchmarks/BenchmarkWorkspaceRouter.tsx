5dd40099b9d81a418b2d73d672557c820ba08804
saga0003 <sagar@smis.edu.in>
2026-07-19 21:56:59 +0530

    Add cloud-aware benchmark governance routing

--- src/components/benchmarks/BenchmarkWorkspaceRouter.tsx
+++ src/components/benchmarks/BenchmarkWorkspaceRouter.tsx
@@ -5,6 +5,9 @@ import { CloudSchoolBenchmarkWorkspace } from "./CloudSchoolBenchmarkWorkspace";
 import { SchoolBenchmarkWorkspace } from "./SchoolBenchmarkWorkspace";
 import { CloudStudentBenchmarkWorkspace } from "./CloudStudentBenchmarkWorkspace";
 import { StudentBenchmarkWorkspace } from "./StudentBenchmarkWorkspace";
+import { CloudBenchmarkGovernance } from "./CloudBenchmarkGovernance";
+import { BenchmarkGovernance } from "./BenchmarkGovernance";
 
 export function SchoolBenchmarkRouter(){return isSupabaseConfigured?<CloudSchoolBenchmarkWorkspace/>:<SchoolBenchmarkWorkspace/>}
 export function StudentBenchmarkRouter(){return isSupabaseConfigured?<CloudStudentBenchmarkWorkspace/>:<StudentBenchmarkWorkspace/>}
+export function AdminBenchmarkRouter(){return isSupabaseConfigured?<CloudBenchmarkGovernance/>:<BenchmarkGovernance/>}
