871f78be235a0b154eb72171c671cb4928197876
saga0003 <sagar@smis.edu.in>
2026-07-27 07:04:04 +0530

    Integrate V12 analytics views with home navigation

--- src/app/student/analytics/page.tsx
+++ src/app/student/analytics/page.tsx
@@ -1,5 +1,6 @@
 import { ProtectedPage } from "@/components/ProtectedPage";
+import { AnalyticsV12Frame } from "@/components/analytics/AnalyticsV12Frame";
 
 export default function Page(){
-  return <ProtectedPage allowed="student"><iframe title="Evidara V12 complete student analytics" src="/evidara-analytics-v12.html" style={{position:"fixed",inset:0,width:"100%",height:"100%",border:0,background:"#fbfcfd"}} /></ProtectedPage>;
+  return <ProtectedPage allowed="student"><AnalyticsV12Frame/></ProtectedPage>;
 }
