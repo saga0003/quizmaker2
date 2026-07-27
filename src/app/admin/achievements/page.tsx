5080457022bbd0626ae065fb936cf6f1d91a1ee4
saga0003 <sagar@smis.edu.in>
2026-07-27 08:12:39 +0530

    Remove admin achievements route from Evidara 11 baseline

--- src/app/admin/achievements/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardShell } from "@/components/DashboardShell";
-import { ProtectedPage } from "@/components/ProtectedPage";
-import { AdminAchievementGovernance } from "@/components/achievements/AdminAchievementGovernance";
-
-export default function Page(){return <ProtectedPage allowed="admin"><DashboardShell kind="admin"><AdminAchievementGovernance/></DashboardShell></ProtectedPage>}
