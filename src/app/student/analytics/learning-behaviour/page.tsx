990c6bb29b407fb34daaf78de2e53c2241d423db
saga0003 <sagar@smis.edu.in>
2026-07-27 08:11:42 +0530

    Remove learning behaviour analytics route from Evidara 11 baseline

--- src/app/student/analytics/learning-behaviour/page.tsx
+++
@@ -1,7 +0,0 @@
-import { DashboardShell } from "@/components/DashboardShell";
-import { ProtectedPage } from "@/components/ProtectedPage";
-import { LearningBehaviourInsights } from "@/components/analytics/LearningBehaviourInsights";
-
-export default function Page(){
-  return <ProtectedPage allowed="student"><DashboardShell kind="student"><LearningBehaviourInsights/></DashboardShell></ProtectedPage>;
-}
