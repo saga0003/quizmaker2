bf903dad1e5ca3c76aec689a12ff26e312e101d3
saga0003 <sagar@smis.edu.in>
2026-07-27 08:12:05 +0530

    Remove student benchmarks route from Evidara 11 baseline

--- src/app/student/benchmarks/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardShell } from "@/components/DashboardShell";
-import { ProtectedPage } from "@/components/ProtectedPage";
-import { StudentBenchmarkRouter } from "@/components/benchmarks/BenchmarkWorkspaceRouter";
-
-export default function Page(){return <ProtectedPage allowed="student"><DashboardShell kind="student"><StudentBenchmarkRouter/></DashboardShell></ProtectedPage>}
