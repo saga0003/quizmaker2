d72a9138f7112eb8b5dd2af9d57562094bd83d6d
saga0003 <sagar@smis.edu.in>
2026-07-27 08:12:26 +0530

    Remove school benchmarks route from Evidara 11 baseline

--- src/app/school/benchmarks/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardShell } from "@/components/DashboardShell";
-import { ProtectedPage } from "@/components/ProtectedPage";
-import { SchoolBenchmarkRouter } from "@/components/benchmarks/BenchmarkWorkspaceRouter";
-
-export default function Page(){return <ProtectedPage allowed="school"><DashboardShell kind="school"><SchoolBenchmarkRouter/></DashboardShell></ProtectedPage>}
