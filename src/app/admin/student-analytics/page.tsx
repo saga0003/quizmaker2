587f209cedd818c732c34d85e8e94fdb2af5907b
saga0003 <sagar@smis.edu.in>
2026-07-27 08:11:29 +0530

    Remove admin student analytics route from Evidara 11 baseline

--- src/app/admin/student-analytics/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProtectedPage } from "@/components/ProtectedPage";
-import { DashboardShell } from "@/components/DashboardShell";
-import { V12LiveAnalytics } from "@/components/analytics/V12LiveAnalytics";
-
-export default function Page(){
-  return <ProtectedPage allowed="admin"><DashboardShell kind="admin"><V12LiveAnalytics mode="admin"/></DashboardShell></ProtectedPage>;
-}
